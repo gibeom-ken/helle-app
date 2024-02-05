826e2db0a98385ed17e2cdbff9c8d0c5127b30bd
gibeom-ken <158978093+gibeom-ken@users.noreply.github.com>
2024-02-05 18:01:08 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
-from pygwalker.api.streamlit import StreamlitRenderer, init_streamlit_comm
 import pandas as pd
+import streamlit.components.v1 as components
 import streamlit as st
-
+from pygwalker.api.streamlit import init_streamlit_comm, get_streamlit_html
 
 st.write('Hello World!')
