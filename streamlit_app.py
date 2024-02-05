5d6da1990c08b8a1d3575401ec3635fda6a96e74
gibeom-ken <158978093+gibeom-ken@users.noreply.github.com>
2024-02-05 18:15:03 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,31 @@
+from pygwalker.api.streamlit import StreamlitRenderer, init_streamlit_comm
 import pandas as pd
-import streamlit.components.v1 as components
 import streamlit as st
-from pygwalker.api.streamlit import init_streamlit_comm, get_streamlit_html
 
-st.write('Hello World!')
+
+ 
+# Adjust the width of the Streamlit page
+st.set_page_config(
+    page_title="API데이터솔루션 활용하기",
+    layout="wide"
+)
+
+# Establish communication between pygwalker and streamlit
+init_streamlit_comm()
+
+# Add Title
+st.title("API데이터솔루션 활용하기")
+st.image("C안_2.png")
+
+# Get an instance of pygwalker's renderer. You should cache this instance to effectively prevent the growth of in-process memory.
+@st.cache_resource
+def get_pyg_renderer() -> "StreamlitRenderer":
+    df = pd.read_csv("https://kanaries-app.s3.ap-northeast-1.amazonaws.com/public-datasets/bike_sharing_dc.csv")
+    # When you need to publish your app to the public, you should set the debug parameter to False to prevent other users from writing to your chart configuration file.
+    return StreamlitRenderer(df, spec="./gw_config.json", debug=False)
+ 
+renderer = get_pyg_renderer()
+ 
+# Render your data exploration interface. Developers can use it to build charts by drag and drop.
+renderer.render_explore()
+ 
